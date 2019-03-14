8b73c284c7423d496fc5fec589bb9bbfba9deb35
JangGeunSong <sjg1468@gmail.com>
2019-03-14 19:15:34 +0900

    DB link and test move TMP

--- server/server.js
+++ server/server.js
@@ -2,38 +2,21 @@ const http = require('http');
 const fs = require('fs');
 const template = require('../view/form/temlplate');
 const path = require('path');
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
 
-// Connection URL
-const dbURL = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'myproject';
-
-// Use connect method to connect to the server
-MongoClient.connect(dbURL, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  client.close();
-});
-
+// server create
 var app = http.createServer((request, response) => {
-    var url = request.url;
+    var url = request.url; //request url
     console.log(url);
     
-    if(url === '/') {
+    if(url === '/') { //If request index page
         response.writeHead(200);
-        response.end(template.postTmp());
+        response.end(template.postTmp()); //post board call
     }
     else if(url.match('.css')){
         var cssPath = path.join('../webApp_nodejs/view', '', request.url);
         var fileStream = fs.createReadStream(cssPath);
         response.writeHead(200, {'Content-Type' : 'text/css'});
-        fileStream.pipe(response);
+        fileStream.pipe(response); //link to the html template use CSS file
     }
     else if(url.match('.jpg')) {
         var imgPath = path.join('../webApp_nodejs/view', '', request.url);
@@ -41,6 +24,7 @@ var app = http.createServer((request, response) => {
         response.writeHead(200, {'Content-Type' : 'text/jpg'});
         fileStream.pipe(response);
     }
-});
+}); // define create server
 
 app.listen(3000);
+//web application listen localhost:3000
--- view/form/temlplate.js
+++ view/form/temlplate.js
@@ -1,3 +1,40 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const dbURL = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'mycustomers';
+
+// Use connect method to connect to the server
+MongoClient.connect(dbURL, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  findDocuments(db, () => {
+    console.log("Find Document is success!");
+  })
+
+  client.close();
+});
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('customers');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    docs.forEach(element => {
+      console.log(element.gender);
+    });
+    callback(docs);
+  });
+}
+
 exports.postTmp = () => {
     return `
     <!DOCTYPE html>
@@ -48,3 +85,4 @@ exports.postTmp = () => {
     </html>
     `
 };
+//post board template
